cac2ad50d280d1f247e12d67fdd5f1df3fd041f1
Sergio Asuero Díaz <murtiuso@MBPro2017.local>
2017-11-03 12:40:36 +0100

    Separación de tareas en archivos

--- js/Clases.js
+++ js/Clases.js
@@ -189,29 +189,7 @@ if(typeof year != "string") {
 
 }
 
-// creación de objetos de prueba
-// por qué se pone en undefined el name?
 
-// crear array para los géneros
-let cancion1 = new Song("Orobroy","Dorantes", "Orobroy", "Flamenco",  "1998", 4, "Flamenco con piano");
-let cancion2 = new Song("Hippaly","Hippaly", "El Surco Responde al Silencio", "Hip Hop instrumental",  "1997", 3, "En su versión con voz eran DPC & K");
-let cancion3 = new Song("Turret Opera (A Cappella) [feat. Liz Baseman]","Acquire A Cappella", "Turret Opera (A Cappella) [feat. Liz Baseman] - Single", "BSO",  "2011", 4, "Banda sonora del juego portal 2");
-let cancion4 = new Song("1 in 100,000","L-FRESH The LION", "Become", "Hip Hop",  "2016", 5, "Artista australiano");
-
-
-// por qué cancion1 ahora no se muestra corectamente
-console.log(cancion1); // esto funciona y muestra loa valores del objeto
-// por qué ahora esto no me muestra el valor de name?, ¿cómo se hace un paso a paso en WS?
-console.log(cancion1.getName());
-
-// settear nombre
-/*cancion1.setName(1);
-cancion1.setName("");
-cancion1.setName("lalala");*/
-
-console.log(cancion2);
-console.log(cancion3);
-console.log(cancion4);
 
 
 /*
---
+++ js/Edicion.js
@@ -0,0 +1,8 @@
+/*
+
+
+Función que valide todos los campos de una canción.
+Hay que tener en cuenta que los campos no pueden ser nulos, undefined ni vacios, una canción debe tener al menos 1 cantante.
+Y los cantantes, deben tener un nombre y apellidos.
+Y una canción debe pertenecer al menos a un disco de música.
+ */
